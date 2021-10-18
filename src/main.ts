78061a885c296977fd7f70fb37c3d1f1394703ae
sijune <sijune0525@gmail.com>
2021-10-18 22:40:13 +0900

    PORT

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors(); //CORS 에러 해결
-  await app.listen(4000); //프론트를 3000에서 돌릴 예정이므로 포트변경
+  //heroku 배포를 위해 PORT 추가
+  await app.listen(process.env.PORT || 4000); //프론트를 3000에서 돌릴 예정이므로 포트변경
 }
 bootstrap();
